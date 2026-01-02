40bbc7b8fa6be43dcc01dd885c7822047d81e48b
rejaulsheikh612-prog <rejaulsheikh612@gmail.com>
2026-01-02 19:45:04 +0600

    Create MainActivity.java

---
+++ app/src/main/java/com/tictactoe/app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tictactoe.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
